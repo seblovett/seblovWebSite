17bfcbead762e8226286be6e2c605c24d34dbfac
seblovett <seblovett@googlemail.com>
2013-08-13 21:17:03 +0100

    Corrected some small errors
    Signed-off-by: seblovett <seblovett@googlemail.com>

--- footer.php
+++ footer.php
@@ -4,8 +4,8 @@
 		echo '<div class="footer">';
 		echo '<ul>';
 			echo '<li><a href="'.$relpath.'index.php">Home</a></li>';
-			echo '<li><a href="'.$relpath.'about.html">About</a></li>';
-			echo '<li><a href="'.$relpath.'Projects.php">Projects</a></li>';
+			//echo '<li><a href="'.$relpath.'about.html">About</a></li>';
+			echo '<li><a href="'.$relpath.'projects.php">Projects</a></li>';
 			echo '<li><a href="'.$relpath.'misc.php">Portfolio</a></li>';
 		echo '</ul>';
 		echo '<p>&nbsp;          	</p>';
