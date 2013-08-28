7957eaa567173753801276f83ab501962335f68f
seblovett <seblovett@googlemail.com>
2013-08-28 20:34:51 +0100

    Added gallery to links. Added two new prints to the gallery.

--- links.php
+++ links.php
@@ -5,6 +5,7 @@
 		echo '<li><a href="'.$relpath.'index.php">Home</a></li>';
 		echo '<li><a href="'.$relpath.'awards.php">Awards and Achievements</a></li>';
 		echo '<li><a href="'.$relpath.'projects.php">Projects</a></li>';
+		echo '<li><a href="'.$relpath.'Gallery.php">Gallery</a></li>';
 		echo '<li><a href="'.$relpath.'misc.php">Other Things</a></li>';
 	}
 ?>
